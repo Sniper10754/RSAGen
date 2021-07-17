459600b17dc69f61599eab9db64e5278a3df862d
UnityCoderByte <83960796+UnityCoderByte@users.noreply.github.com>
2021-07-17 13:55:54 +0200

    update

--- rg.cpp
+++ rg.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<conio.h>
 #include<stdlib.h>
-#include <cstdint>
-#include <vector>
-#include <random>
+
+
+
+#include <string.h>
+#include <cstdlib>
 
 
 
@@ -11,20 +13,19 @@
 
 
 using namespace std;
+
+
+
+
+
+
+
 int main(int argc, char **argv) {
-    std::random_device rd;
-    std::uniform_int_distribution<uint32_t> dist(0,0xFFFFFFFFu);
-    std::vector<char> data(1000);
-    int offset = 0;
-    uint32_t bits = 0;
-    for (char& d : data)
-    {
-        if (offset == 0)
-            bits = dist(rd);
-        d = static_cast<char>(bits & 0xFF);
-        bits >>= 8;
-        if (++offset >= 4)
-            offset = 0;
-    }
+    char p[81];
+    char a[] = "abcdefghijklmnopqrstuvwxyz";
+    for (int i=0;i<=80;i++)
+        p[i] = a[rand()%26];
+    p[80] = '\0';
+    cout<<p<<endl;
     return 0;
 }
