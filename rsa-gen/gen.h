e0812bf260c9bf4dbafae3500bf44480fb0cceaf
Sniper10754 <alessioteti28@gmail.com>
2021-07-17 15:13:26 +0200

    Update gen.h

--- rsa-gen/gen.h
+++ rsa-gen/gen.h
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-namespace rsa_gen {
+    namespace rsa_gen {
 
     string encrypt(string textp, int keyp) {
         string text;
@@ -16,46 +16,50 @@ namespace rsa_gen {
         int i, key;
         key = keyp;
         text = textp;
-        for(i = 0; text[i] != '\0'; ++i){
+        for(i = 0; text[i] != '\0'; ++i) {
             temp = text[i];
             //If the message to be encypted is in lower case
             if(temp >= 'a' && temp <= 'z'){
-            temp = temp + key;
-            
-            if(temp > 'z'){
-                temp = temp - 'z' + 'a' - 1;
-            }
-            
-            text[i] = temp;
+                temp = temp + key;
+
+                if(temp > 'z'){
+                    temp = temp - 'z' + 'a' - 1;
+                }
+
+                text[i] = temp;
             }
             //If the message to be encypted is in upper case
             else if(temp >= 'A' && temp <= 'Z'){
-            temp = temp + key;
-            
-            if(temp > 'Z'){
-                temp = temp - 'Z' + 'A' - 1;
-            }
-            
-            text[i] = temp;
-            return text;
+                temp = temp + key;
+
+                if(temp > 'Z'){
+                    temp = temp - 'Z' + 'A' - 1;
+                }
+
+                text[i] = temp;
+                return text;
             }
         }
         return text;
-        }
+    }
 
-        int PBKEY = 2348237;
-        string key() {
+    int PBKEY = 2348237;
+    string key() {
         int alg;
         int max = 120;
         int min = 40;
+
         srand ( time(NULL) );
         alg = rand() % max + min;
+        
         char p[alg];
         char a[] = "abcdefghijklmnopqrstuvwxyz";
         for (int i=0;i<=alg;i++)
-            p[i] = a[rand()%26];
+        
+        p[i] = a[rand()%26];
         p[alg-1] = '\0';
         string PLK = encrypt(p, PBKEY);
+        
         return PLK;
     }
 }
