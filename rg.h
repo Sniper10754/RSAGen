fa9f6098393ac2f295ab729b4e9c11fbf25af12c
UnityCoderByte <83960796+UnityCoderByte@users.noreply.github.com>
2021-07-17 13:38:19 +0200

    file edit

---
+++ rg.h
@@ -0,0 +1,23 @@
+#include <vector>
+#include <random>
+#include <climits>
+#include <algorithm>
+#include <functional>
+
+using random_bytes_engine = std::independent_bits_engine<
+    std::default_random_engine, CHAR_BIT, unsigned char>;
+
+using namespace std;
+
+
+
+
+
+
+int main() { 
+    String data = "Hello World";
+    random_bytes_engine rbe;
+    std::vector<unsigned char> data(1000);
+    std::generate(begin(data), end(data), std::ref(rbe));
+    return 0;
+}
